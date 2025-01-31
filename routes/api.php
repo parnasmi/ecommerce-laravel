6a329fc344963f41d39d8457324da0aa03c2f388
ilkhom_maksadkulov <ilkhom.maksadkulov@opentech.uz>
2025-01-31 10:26:52 +0500

    feat: implement basic Auth and create product_user(favorites) pivot table migration file and relations

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CategoryProductController;
 use App\Http\Controllers\ProductController;
@@ -8,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', [AuthController::class, 'login']);
 
 Route::apiResources([
     'categories' => CategoryController::class,
